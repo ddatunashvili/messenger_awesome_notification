c7b05e124a73295eb2f9b9166a148533a6ee1f5e
ddatunashvili <davitidatunashvili@gmail.com>
2022-12-05 12:49:18 +0400

    georgian lang support

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener("DOMNodeInserted", (event) => {
     try {
     const hasClass = event.target.getAttribute("aria-label") == "Mark as read";
-    if (hasClass) {
+    const hasClassge = event.target.getAttribute("aria-label") == "წაკითხულად მონიშვნა";
+    if (hasClass || hasClassge){
       var myAudio = new Audio(chrome.runtime.getURL("aah.mp3"));
       myAudio.volume = 0.1;
       myAudio.play();
