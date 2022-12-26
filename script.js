0602e66027428c22dd311d58d6557f31673ae245
daviti datunashvili <davitidatunashvili98@gmail.com>
2022-12-26 21:24:20 +0400

    new sound effect

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.addEventListener("DOMNodeInserted", (event) => {
+  console.log(event.target);
     try {
     const hasClass = event.target.getAttribute("aria-label") == "Mark as read";
     if (hasClass) {
-      var myAudio = new Audio(chrome.runtime.getURL("aah.mp3"));
-      myAudio.volume = 0.1;
+      // var myAudio = new Audio(chrome.runtime.getURL("aah.mp3"));
+      var myAudio = new Audio(chrome.runtime.getURL("roblox_death.mp3"));
+      // myAudio.volume = 0.1;
       myAudio.play();
     }
   } catch {}
