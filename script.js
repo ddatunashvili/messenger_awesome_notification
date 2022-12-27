73aea10e6c7b9a4c6f8c44658902607b798dd353
ddatunashvili <davitidatunashvili98@gmail.com>
2022-12-27 12:40:17 +0400

    sound effect added

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.addEventListener("DOMNodeInserted", (event) => {
     try {
     const hasClass = event.target.getAttribute("aria-label") == "Mark as read";
     if (hasClass) {
-      var myAudio = new Audio(chrome.runtime.getURL("aah.mp3"));
-      myAudio.volume = 0.1;
+      var myAudio = new Audio(chrome.runtime.getURL("roblox_death.mp3"));
+      // myAudio.volume = 0.1;
       myAudio.play();
     }
   } catch {}
